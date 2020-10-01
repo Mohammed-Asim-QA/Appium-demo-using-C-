fb31ad70f6454be91bb0b1f40b6667cbb4331d0e
Mohammed-Asim-QA <72217264+Mohammed-Asim-QA@users.noreply.github.com>
2020-10-01 17:13:12 +0100

    Add files via upload

---
+++ Extent.cs
@@ -0,0 +1,88 @@
+﻿using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
+using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+
+namespace AppiumDemo
+{
+    [TestFixture]
+    public class Extent
+    {
+        public ExtentReports extent = new ExtentReports();
+        public ExtentTest test;
+        [OneTimeSetUp]
+        public void StartReport()
+        {
+            string path = System.Reflection.Assembly.GetCallingAssembly().CodeBase;
+            string actualPath = path.Substring(0, path.LastIndexOf("bin"));
+            string projectPath = new Uri(actualPath).LocalPath;
+            string reportPath = projectPath + "Reports\\MyOwnReport.html";
+            //Console.WriteLine("the reports path is {0}", reportPath);
+
+            extent = new ExtentReports();
+            // extent = new ExtentReports(reportPath);
+            /* extent
+             .AddSystemInfo("Host Name", "Mohammed")
+             .AddSystemInfo("Environment", "QA")
+             .AddSystemInfo("User Name", "Test User");
+             extent.LoadConfig(projectPath + "extent-config.xml");*/
+            var dir = AppDomain.CurrentDomain.BaseDirectory.Replace("\\bin\\Debug\\Test_Execution_Reports", "dddd");
+            DirectoryInfo di = Directory.CreateDirectory(dir + "\\Test_Execution_Reports");
+            var htmlReporter = new ExtentHtmlReporter(dir + "\\Test_Execution_Reports" + "\\Automation_Report" + "MyOwnReport.html");
+            extent.AddSystemInfo("Environment", "Leeds Digital Festival");
+            extent.AddSystemInfo("User Name", "Mohammed Asim");
+            extent.AddSystemInfo("AppName", "Appium Demo");
+            extent.AttachReporter(htmlReporter);
+           
+
+
+        }
+
+
+        //[Test]
+        //public void DemoReportPass()
+        //{
+        //    //test = extent.StartTest("DemoReportPass");
+        //    test = extent.CreateTest("DemoReportsPass");
+        //    Assert.IsTrue(true);
+        //    test.Log(Status.Pass, "Assertion Pass as condition is true");
+        //   // test.Log(LogStatus.Pass, "Assert Pass as condition is True");
+        //}
+
+        //[Test]
+        //public void DemoReportFail()
+        //{
+        //    test = extent.CreateTest("DemoReportFail");
+        //    Assert.IsTrue(false);
+        //    test.Log(Status.Pass, "Assert Fail as condition is False");
+        //}
+
+        [TearDown]
+        public void GetResult()
+        {
+            var status = TestContext.CurrentContext.Result.Outcome.Status;
+            var stackTrace = "<pre>" + TestContext.CurrentContext.Result.StackTrace + "</pre>";
+            var errorMessage = TestContext.CurrentContext.Result.Message;
+
+            if (status == TestStatus.Failed)
+            {
+                test.Log(Status.Fail, stackTrace + errorMessage);
+            }
+            
+            //extent.EndTest(test);
+        }
+
+        [OneTimeTearDown]
+        public void EndReport()
+        {
+            extent.Flush();
+            // extent.Close(); not working
+        }
+
+    }
+}
---
+++ UnitTest1.cs
@@ -0,0 +1,99 @@
+using AventStack.ExtentReports;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium.Appium.Enums;
+using OpenQA.Selenium.Appium.Interfaces;
+using OpenQA.Selenium.Appium.MultiTouch;
+using OpenQA.Selenium.Interactions;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace AppiumDemo
+{
+    public class Tests : Extent
+    {
+        private static AndroidDriver<AndroidElement> _driver;
+        
+        [SetUp]
+        public void Setup()
+        {
+
+            var options = new AppiumOptions { PlatformName = "Android" };
+            options.AddAdditionalCapability("deviceName", "Pixel 2 API 30");
+            //options.AddAdditionalCapability(MobileCapabilityType.App, appPath);
+            // add app or appPackage / appActivity depending on preference to use a hybrid ap
+            options.AddAdditionalCapability("udid", "emulator-5554");
+            options.AddAdditionalCapability("automationName", "UiAutomator2"); // this one is important
+            options.AddAdditionalCapability("appPackage", "com.etsy.android");
+            options.AddAdditionalCapability("appActivity", "com.etsy.android.ui.homescreen.HomescreenTabsActivity");
+
+            // these are optional  -- see DesiredCapabilities Appium docs to learn more
+           
+            options.AddAdditionalCapability("autoGrantPermissions", true);
+            options.AddAdditionalCapability("allowSessionOverride", true);
+            options.AddAdditionalCapability("adbExecTimeout", 30000);
+            options.AddAdditionalCapability("newCommandTimeout", 60);
+           
+            _driver = new AndroidDriver<AndroidElement>(new Uri("http://127.0.0.1:4723/wd/hub"), options);
+            Extent extent = new Extent();
+            extent.StartReport();            
+
+        }
+
+
+        public void TapByCoordinates(int x, int y)
+        {
+            var actions = new TouchAction(_driver);
+            actions.Tap(x, y).Wait(250).Perform();
+
+        }
+
+        [Test]
+        public void Test1()
+        {
+            test = extent.CreateTest("Etsy App demo using Appium");
+            test.Log(Status.Info, "Launching the App");
+            Thread.Sleep(1000);
+            test.Log(Status.Info, "Click Getting started link to Login");
+            _driver.FindElementById("com.etsy.android:id/btn_link").Click();
+            Thread.Sleep(1000);
+            test.Log(Status.Info, "Enter Userid/email");
+            _driver.FindElementById("com.etsy.android:id/edit_username").SendKeys("FlorenceBronte@Outlook.com");
+            Thread.Sleep(1000);
+            test.Log(Status.Info, "Enter Password");
+            _driver.FindElementById("com.etsy.android:id/edit_password").SendKeys("0ctober123");
+            Thread.Sleep(1000);
+            test.Log(Status.Info, "Sign in");
+            _driver.FindElementById("com.etsy.android:id/button_signin").Click();
+            Thread.Sleep(2000);
+            test.Log(Status.Pass, "You have successfully logged in");
+            Thread.Sleep(2000);
+            test.Log(Status.Pass, "Click Favourites tab");
+            TapByCoordinates(327, 1707);
+            Thread.Sleep(2000);
+            test.Log(Status.Pass, "Click Updates tab");
+            TapByCoordinates(539, 1712);
+            Thread.Sleep(2000);
+            string msg = _driver.FindElementById("com.etsy.android:id/first_notification_text").Text;
+            Console.WriteLine("Notification message displayed: {0}", msg);
+            Thread.Sleep(2000);
+            Assert.AreEqual("Your first notification!",msg);
+            test.Log(Status.Info, "Click Home tab");
+            TapByCoordinates(107, 1712);
+            Thread.Sleep(1000);
+            test.Log(Status.Pass, "Notification message assertion passed! Etsy app demo successfull"); 
+            Assert.Pass();  
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+            _driver.Quit();
+            
+        }
+    }
+}
+ 
